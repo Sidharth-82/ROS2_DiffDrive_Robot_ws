83989d2736671bfeb59b57f2af1bc9831b14d5e0
Sidharth-82 <sidharthsreeram@gmail.com>
2024-04-07 23:17:11 -0400

    5

--- src/diff_drive_robot/launch/launch_sim.launch.py
+++ src/diff_drive_robot/launch/launch_sim.launch.py
@@ -15,10 +15,20 @@ def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    
 
     package_name='diff_drive_robot'
 
+    
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+    
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -48,12 +58,21 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
+    
+    online_async = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','online_async_launch.launch.py'
+                )]), launch_arguments={'slam_params_file': os.path.join(get_package_share_directory(package_name),'config','mapper_params_online_async.yaml'),
+                                       'use_sim_time': 'true'}.items()
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
+        twist_mux,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
+        online_async
     ])
